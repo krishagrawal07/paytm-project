eb56c8a743fd6b54e444064e8fbea3d50b56c0c5
krish agrawal <krishagrawal706krish07@gmail.com>
2026-04-22 19:56:18 +0530

    Delete backend directory

--- backend/utils/generateToken.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const generateToken = (email) => {
-  const jwtSecret = process.env.JWT_SECRET || "supersecretkey";
-  return jwt.sign({ email, role: "admin" }, jwtSecret, { expiresIn: "1d" });
-};
-
-export default generateToken;
